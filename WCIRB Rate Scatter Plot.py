bbc574712cb31786bf91ab03ffde283a4eda98cb
Alex Williams <100657614+alexwilliams48@users.noreply.github.com>
2022-08-15 09:47:44 -0700

    Added Comments

--- WCIRB Rate Scatter Plot.py
+++ WCIRB Rate Scatter Plot.py
@@ -19,11 +19,11 @@ all_files = (r"C:\....\...\...\...\approved_01012018_pure_premium_rates.csv",  #
             r"C:\\....\...\...\...\\jan_1_2019_approved_pure_premium_rates.csv", ## Full file location hidden for priacy concerns
             r"C:\\....\...\...\...\\approved_01012020_pure_premium_rates.csv",    ## Full file location hidden for priacy concerns
             r"C:\\....\...\...\...\aapproved_09012021_pure_premium_rates.csv")    ## Full file location hidden for priacy concerns
-rate_dict = {}
-for f in all_files:
-    df = pd.read_csv(f, header = None)
+rate_dict = {}  #Create dictionary
+for f in all_files:   
+    df = pd.read_csv(f, header = None)  ##creates a new df for each iteration
     for i in df.index:    
-        rate_dict.setdefault(df.loc[i, 0], []).append(df.loc[i, 1])
+        rate_dict.setdefault(df.loc[i, 0], []).append(df.loc[i, 1])  ##appends dictionary for each item in dataframe
         
 
 
@@ -32,7 +32,7 @@ for f in all_files:
 
 def rate_scat(x):
     
-    dates = ["2018-01-10", "2018-01-07", "2019-01-10", "2020-01-10", "2021-01-10"]
+    dates = ["2018-01-10", "2018-01-07", "2019-01-10", "2020-01-10", "2021-01-10"]  ##Releate dates for each rate
     
 
     y = rate_dict[x]
